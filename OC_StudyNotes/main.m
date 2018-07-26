e140c733b78e6eaa3b409d211ef3237f7f27cc2f
batu <304701204@qq.com>
2018-07-26 11:08:43 +0800

    Initial Commit

---
+++ OC_StudyNotes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC_StudyNotes
+//
+//  Created by 张智 on 2018/7/26.
+//  Copyright © 2018年 gvitech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
